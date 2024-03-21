0d2b50cc5dd6c6489744a24aded2c8f8309ea7b6
Yahya <YAHYA@DESKTOP-4QIIH5S>
2024-03-21 11:57:33 +0300

    Add project files.

---
+++ EntityLayer/Concrete/Message.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Message
+    {
+        public int MessageId { get; set; }
+        public string Subject { get; set; }
+        public string Content { get; set; }
+        public string RecieverMail { get; set; }
+        public int AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
+        public DateTime SendDate { get; set; }
+        public DateTime DeletedDate { get; set; }
+        public bool Status { get; set; }
+        public bool IsRead { get; set; }
+    }
+}
